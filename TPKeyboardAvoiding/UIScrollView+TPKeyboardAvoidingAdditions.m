1a66d0e2b95f52feedd7d98df5311e690fa6448b
Ricardo Santos <rics@ntos.me>
2018-06-14 13:22:22 +0930

    Tweak the bottom content inset to preserve the original inset

--- TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
+++ TPKeyboardAvoiding/UIScrollView+TPKeyboardAvoidingAdditions.m
@@ -339,7 +339,7 @@ static const int kStateKey;
     TPKeyboardAvoidingState *state = self.keyboardAvoidingState;
     UIEdgeInsets newInset = self.contentInset;
     CGRect keyboardRect = state.keyboardRect;
-    newInset.bottom = keyboardRect.size.height - MAX((CGRectGetMaxY(keyboardRect) - CGRectGetMaxY(self.bounds)), 0);
+    newInset.bottom = MAX(keyboardRect.size.height, state.priorInset.bottom) - MAX((CGRectGetMaxY(keyboardRect) - CGRectGetMaxY(self.bounds)), 0);
     return newInset;
 }
 
